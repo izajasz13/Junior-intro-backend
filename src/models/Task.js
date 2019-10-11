83a692442b44acd06d6c54721080de5e383b0a53
izajasz13 <karoldd13@gmail.com>
2019-10-11 22:02:05 +0200

    Add task model and validation

---
+++ src/models/Task.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const taskSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        min: 4
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    coins: {
+        type: Number,
+        required: true
+    },
+    exp: {
+        type: Number,
+        required: true
+    },
+    question: [],
+    answers: []
+});
+
+const Task = mongoose.model('Task', taskSchema);
+
+function validateTask(task){
+    const schema = {
+        title: Joi.string().min(4).required(),
+        description: Joi.string().required(),
+        coins: Joi.number().required(),
+        exp: Joi.number().required()
+    }
+    return Joi.validate(task, schema)
+};
+
+exports.taskSchema = taskSchema;
+exports.validateTask = validateTask;
+exports.Task = Task;
